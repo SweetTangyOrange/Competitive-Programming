34eb929f14a9beff7012b3d6dc8a0ee070c6cd07
SweetTangyOrange <abhirajmallangi@outlook.com>
2024-08-01 16:08:24 -0400

    Add files via upload

---
+++ Codeforces/maximizetheroot.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Abhiraj Mallangi on 7/30/24.
+//
---
+++ Codeforces/template.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+#define tc int t {}; cin >> t; while (t--) {solve();}
+#define io ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+typedef long long ll;
+typedef vector<vector<int>> matrix;
+
+void solve() {
+
+}
+
+int main() {
+    io
+    tc
+
+    return 0;
+}
