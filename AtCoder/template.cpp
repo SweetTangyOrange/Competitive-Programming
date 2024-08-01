f4ac78f435edad0a78a3a92b440d253914bdb3cc
SweetTangyOrange <abhirajmallangi@outlook.com>
2024-08-01 16:07:59 -0400

    Add files via upload

---
+++ AtCoder/template.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+#define io ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+typedef long long ll;
+typedef vector<vector<int>> matrix;
+
+int main() {
+    io
+
+
+    return 0;
+}
